211ccec173a770345a19a842966c040c80793fbc
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2013-04-12 20:50:51 +0530

    Database connectivity class

--- dbconnect.php
+++ dbconnect.php
@@ -18,7 +18,8 @@ class dbconnect {
     function queryAll() {
         $con = mysql_connect($this->host, $this->user, $this->pass);
         mysql_select_db($this->dbname, $con);
-//Query to select all tuples from the table
+		
+		//Query to select all tuples from the table
         $query = "select * from entries";
         $result = mysql_query($query, $con);
         return $result;
@@ -29,7 +30,11 @@ class dbconnect {
         $con = mysql_connect($this->host, $this->user, $this->pass);
         mysql_select_db($this->dbname, $con);
         $res=mysql_query($query,$con);
-        return $result;
+        
+		$conn = mysqli_connect($this->host, $this->user, $this->pass, $this->dbname) or die("failed to connect");
+        $res = mysqli_query($conn, $query);
+		
+		return $res;
     }
 
 }
